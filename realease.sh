cb6e46916c3556c8ca7833cb5feef7cef7a6499e
root <ppslinux@126.com>
2021-06-01 16:49:34 +0800

    add all

--- realease.sh
+++ realease.sh
@@ -7,14 +7,16 @@ for list in $root_dir/TCR8Collect/  $root_dir/TCR8SendCard/
 do 
 	cd $root_dir
 	cd $list
-	make 
+	make cc
+	make SET=xinjiang
+	make cc
+	make SET=pc
 	cp ./lib*.so $release_dir
 	cp ./*.h $release_dir
 	cp *.pdf $release_dir
 	cp LinuxDemo* -rf $release_dir
 	make cc
 done 
-
 cd $root_dir
 cp ./utils/wintype.h $release_dir
 
